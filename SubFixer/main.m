40fc1290aa61697335ac1e4b4fbc74bc9b08c958
Ahmad <sahmmadh@gmail.com>
2016-08-24 23:58:50 +0430

    Initial Commit

---
+++ SubFixer/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  SubFixer
+//
+//  Created by Ahmad on 8/24/16.
+//  Copyright © 2016 Ahmad. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
